0704320da8e30f0c297ced6358dc82421911147b
Joe Forshaw <joe.d.forshaw@gmail.com>
2018-10-28 10:18:27 +0000

    Added SMS integration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,5 @@
+require 'clockwork'
+
 class UsersController < ApplicationController
   def new
     @user = User.new
@@ -6,7 +8,12 @@ class UsersController < ApplicationController
   def create
     @user = User.new(create_params)
     if @user.save
-      flash[:success] = "Thank you ol' chap! You'll receive your first recommendation soon..."
+      response = send_sms
+      if response.success
+        flash[:success] = "Thank you ol' chap! You'll receive your first recommendation soon..."
+      else
+        flash[:error] = "#{response.error_code}: #{response.error_description}"
+      end
     end
     redirect_to '/'
   end
@@ -17,7 +24,28 @@ class UsersController < ApplicationController
     params.require(:user).permit(:phone_number)
   end
 
-  def send_recommendation
-    
+  def send_sms
+    api = Clockwork::API.new('4dbe78968ca4bf59d789acdd1956570c95f64db4')
+    message = api.messages.build(to: @user.phone_number, content: sms_body)
+    message.deliver
+  end
+
+  def sms_body
+    name = MovieName.order('RANDOM()').first.name
+    summary = MovieSummary.order('RANDOM()').first.summary
+    "\"#{name}\" is showing today #{time} at #{cinema}. #{summary}"
+  end
+
+  def cinema
+    [
+      'ODEON Great Northern',
+      'Printworks Vue',
+      'Lowry Vue',
+      'HOME Manchester'
+    ].sample
+  end
+
+  def time
+    "#{rand(10..23)}:#{['00','15','30','45'].sample}"
   end
 end
